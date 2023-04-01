5bb78eac0f250caead94fd0a590991565c6fbd55
Tomáš Doischer <tomas.p.doischer@gmail.com>
2023-04-01 10:51:53 +0200

    initial generated project

---
+++ src/main/java/com/tomdoischer/phrasedemo/PhraseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tomdoischer.phrasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhraseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhraseDemoApplication.class, args);
+	}
+
+}
